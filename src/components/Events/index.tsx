58903609ffa1e07932c76bbd1655d13761aca054
a1dana04 <ajdanazumakmatova@gmail.com>
2024-07-22 17:00:10 +0600

    events integration

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -1,35 +1,68 @@
-import React from "react";
-import event from "../../components/assets/img/events1.webp";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import EventsList from "./EventsList";
+
 const Events = () => {
+
+  const events_url = 'http://3.38.98.134/events'
+  const useFetch = ({
+    url
+  } = {
+    url: events_url,
+  }) => {
+    const [data, setData] = useState<any>([])
+    const [loading, setLoading] = useState(false)
+  
+    const fetchData = async () => {
+        setLoading(true)
+        try{
+            const response = await fetch(url)
+            const data = await response.json()
+            if(data.statusCode === 200) {
+                setData(data.data);
+            }
+        } catch(error) {
+            console.log(error);
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+  
+    useEffect(() =>{
+        fetchData()
+    }, [])
+  
+    return {data, loading}
+  }
+  
+  const  { data, loading } = useFetch();
+  console.log(data, 'events');
+  
+  if (loading) {
+    return <div>Loading</div>;
+  }
+  
   return (
     <div id="events">
       <div className="container">
         <div className="btn1">
           <button>Добавить мероприятиe</button>
         </div>
-        <div className="events">
-          <div className="events--foto">
-          <Link to={"/detalmerop"}>  <img src={event} alt="img" /></Link>
-            <div className="events--foto__text">
-              <h4>
-                25 августа 2024 <span>17:00</span>
-              </h4>
-              <h1>InkubasiaLAB 2024’s Machine Learning AI Bootcamp</h1>
+        {data &&
+                  data.map((el: any, index: number) => {
+                    return (
+                      <EventsList
+                        key={index}
+                        location={el.location}
+                        name={el.name}
+                        organization_name={el.organization_name}
+                        date={el.date}
+                        
+                      />
+                    );
+                  })}
 
-              <div className="events--foto__text--par">
-                <div className="events--foto__text--par__kor1">
-                  <h6>Организатор</h6>
-                  <h2>InkubasiaLAB</h2>
-                </div>
-                <div className="events--foto__text--par__kor2">
-                  <h6>Когда</h6>
-                  <h2>Коворкинг-резорт ololoAkJol</h2>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
