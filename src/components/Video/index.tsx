61fbb597d0d214a57de0d67bd75daaf143a99331
a1dana04 <ajdanazumakmatova@gmail.com>
2024-07-24 18:42:12 +0600

    organization-integ

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,59 +1,54 @@
-import React from 'react';
+import React from "react";
 
-import video1 from "../assets/img/video1.webp"
-import video2 from "../assets/img/video3.webp"
-import video3 from "../assets/img/video2.webp"
-import video4 from "../assets/img/video4.webp"
-import { Link } from 'react-router-dom';
-import useFetch from '../../hooks/useFetch';
-import VideoList from './VideoList';
-
-
-
+import video1 from "../assets/img/video1.webp";
+import video2 from "../assets/img/video3.webp";
+import video3 from "../assets/img/video2.webp";
+import video4 from "../assets/img/video4.webp";
+import { Link } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import VideoList from "./VideoList";
+import loading1 from "../../components/assets/img/loiding.svg";
 
 const Video = () => {
-    const meetups_url = "http://3.38.98.134/meetups"
+  const meetups_url = "http://3.38.98.134/meetups";
 
-    const  { data, loading } = useFetch({
-        url:meetups_url
-      })
-      console.log(data, 'meetups');
-      
-      if (loading) {
-        return <div>Loading</div>;
-      }
-    return (
-        <div id='video'>
-            <div className="container">
-          <div className="btn">
-          <button >Добавить видео</button>
-          </div>
+  const { data, loading } = useFetch({
+    url: meetups_url,
+  });
+  console.log(data, "meetups");
 
-          <div className="text">
-         <h1>Все видео</h1>
-          </div>
-          <div className="video-card">
-          {data &&
-                  data.map((el: any, index: number) => {
-                    return (
-                      <VideoList
-                        key={index}
-                        title={el.title}
-                        organization_name={el.organization_name}
-                        cover={el.cover}
-                        date={el.date}
-                       
-                        
-                      />
-                    );
-                  })}
-          </div>
-         
-
-            </div>
-
+  if (loading) {
+    return <div className='loading'style={{
+      display:'flex',alignItems:'center',justifyContent:"center"
+  }}><img src={loading1} alt="img" /></div>;
+  }
+  return (
+    <div id="video">
+      <div className="container">
+        <div className="btn">
+          <button>Добавить видео</button>
         </div>
-    );
+
+        <div className="text">
+          <h1>Все видео</h1>
+        </div>
+        <div className="video-card">
+          {data &&
+            data.map((el: any, index: number) => {
+              return (
+                <VideoList
+                  key={index}
+                  title={el.title}
+                  organization_name={el.organization_name}
+                  cover={el.cover}
+                  date={el.date}
+                />
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Video;
