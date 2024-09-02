8ff61bf3dbc7bb2e0ffa74949be0c5efebaaf07b
a1dana04 <ajdanazumakmatova@gmail.com>
2024-09-02 17:28:27 +0600

    t

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -2,10 +2,7 @@ import { log } from "console";
 import { useEffect, useState } from "react";
 import { FaLess } from "react-icons/fa";
 
-import { API } from "../constants/intex"
-
-
-
+import { API } from "../constants/intex";
 
 const useFetch = (
   { url } = {
@@ -18,14 +15,14 @@ const useFetch = (
   const fetchData = async () => {
     setLoading(true);
     try {
-      const response = await fetch(url); const data = await response.json();
+      const response = await fetch(url);
+      const data = await response.json();
       if (data.statusCode === 200) {
         console.log(data);
 
         setData(data.data.sort((a: any, b: any) => b.id - a.id));
       }
     } catch (error) {
-      console.log(error);
     } finally {
       setLoading(false);
     }
